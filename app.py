df79dd60f252a3d16b1be0abae42ae0060ae4e38
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-06-15 19:07:40 +0200

    register and login added

--- app.py
+++ app.py
@@ -25,9 +25,16 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return  redirect(url_for('home'))
+        else:
+            flash('login Unsuccessful. Please check username and password', 'danger')
+
     return render_template('login.html', title='Login', form=form)
 
 
