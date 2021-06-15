b116a89d8d047721f6891d5acab12a0109c89f70
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-06-15 18:16:59 +0200

    register and login added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456789'
 
 
 @app.route('/')
@@ -12,6 +14,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='about')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
