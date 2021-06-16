e4667144eee30a21181ed8ec3145fc70f924ce9a
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2021-06-16 11:40:27 +0200

    Upload profile pic feature has been added

--- scooteq/routes.py
+++ scooteq/routes.py
@@ -3,6 +3,8 @@ from scooteq import app, db, bcrypt
 from scooteq.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from scooteq.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
 
 
 @app.route('/')
@@ -54,11 +56,24 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/pictures', picture_filename)
+    form_picture.save(picture_path)
+
+    return picture_filename
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
